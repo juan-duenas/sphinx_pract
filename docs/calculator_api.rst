65859dd117f99a967c93d2cb74d2be015c47abbd
Juan F. Dueñas <62545646+juan-duenas@users.noreply.github.com>
2025-11-19 12:17:53 +0100

    feat: setup CI/CD with test gate and sphinx documentation

---
+++ docs/calculator_api.rst
@@ -0,0 +1,11 @@
+.. docs/calculator_api.rst
+Calculator API Reference
+========================
+
+.. automodule:: calculator
+   :members:
+
+.. autoclass:: calculator.Calculator
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# Find root directory
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'dsr_spx_prt'
+copyright = '2025, Juan F. Dueñas'
+author = 'Juan F. Dueñas'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',      # Supports numpy and google style docstrings
+    'sphinx_rtd_theme',         # Required for the Read the Docs theme
+    'sphinx_autodoc_typehints'  # Uses Python type hints in the docs
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
